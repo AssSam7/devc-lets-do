e5ac82a4be5c13ca2b5ef8c658cd5c204550c2c5
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-11-08 14:40:31 +0530

    Initializing the template

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,29 @@
     <header>
       <h1>#todo</h1>
     </header>
+
+    <main class="content">
+      <div class="buttons">
+        <div class="all">
+          <button class="btn btn__all">
+            All
+          </button>
+          <hr />
+        </div>
+        <div class="active">
+          <button class="btn btn__active">
+            Active
+          </button>
+          <hr />
+        </div>
+        <div class="completed">
+          <button class="btn btn__completed">
+            Completed
+          </button>
+          <hr />
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -11,6 +34,9 @@ export default {
   name: "app",
   data() {
     return {};
+  },
+  methods: {
+
   }
 };
 </script>
@@ -19,7 +45,48 @@ export default {
 h1 {
   font-family: "Raleway", sans-serif;
   font-style: normal;
+  font-size: 3rem;
   font-weight: 700;
   color: #333333;
+  text-align: center;
+}
+
+main {
+  max-width: 70%;
+  padding: 6rem 0;
+  margin: auto;
+}
+
+.buttons {
+  border-bottom: 1px solid #bdbdbd;
+
+  display: flex;
+  justify-content: space-around;
+}
+
+.btn {
+  color: #333333;
+  border: 0;
+  outline: none;
+  background: none;
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1.4rem;
+}
+
+.all,
+.active,
+.completed {
+  hr {
+    height: 2px;
+    color: transparent;
+    background: #2f80ed;
+    border-radius: 4px 4px 0px 0px;
+  }
 }
 </style>
